d0e5272538926572e84561b83fd0f44b372a780d
Natalia H <nholea@gmail.com>
2022-08-21 18:34:55 +0200

    Add grid and cell class

--- app/src/main/java/Conway/game/of/life/Cell.java
+++ app/src/main/java/Conway/game/of/life/Cell.java
@@ -1,4 +1,31 @@
 package Conway.game.of.life;
 
 public class Cell {
+    private int colPosition;
+    private int rowPosition;
+    private boolean isAlive;
+
+    public Cell(int colPosition, int rowPosition, boolean isAlive) {
+        this.colPosition = colPosition;
+        this.rowPosition = rowPosition;
+        this.isAlive = isAlive;
+    }
+
+    public boolean getState() {
+        return isAlive;
+    }
+
+    public void makeAlive() {
+        isAlive = true;
+    }
+
+    public void makeDead() {
+        isAlive = false;
+    }
+
+    public static void main(String[] args){
+        Cell cell = new Cell(1, 4, true);
+        System.out.println(cell.getState());
+
+    }
 }
--- app/src/main/java/Conway/game/of/life/Grid.java
+++ app/src/main/java/Conway/game/of/life/Grid.java
@@ -1,4 +1,30 @@
 package Conway.game.of.life;
 
 public class Grid {
+    private int rows;
+    private int cols;
+    private String[][] grid;
+
+    public Grid(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new String[rows][cols];
+    }
+
+    public void buildGrid() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                grid[i][j] = ".";
+                System.out.print(grid[i][j] + "\s");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args){
+        Grid grid = new Grid(4,4);
+        grid.buildGrid();
+
+    }
 }
