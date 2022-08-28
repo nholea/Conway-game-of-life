a4f7d393fd01ddf96133dd23ab8a9a57af6f19f7
Natalia H <nholea@gmail.com>
2022-08-28 12:10:38 +0200

    chore: Count alive neighbours and create a next generations of cells

--- app/src/main/java/Conway/game/of/life/Grid.java
+++ app/src/main/java/Conway/game/of/life/Grid.java
@@ -1,30 +1,107 @@
 package Conway.game.of.life;
 
-public class Grid {
-    private int rows;
-    private int cols;
-    private String[][] grid;
+import java.util.Random;
+import java.util.Scanner;
 
-    public Grid(int rows, int cols) {
-        this.rows = rows;
-        this.cols = cols;
-        this.grid = new String[rows][cols];
+public class Grid {
+    private final int rows;
+    private final int cols;
+
+    private int[][] board;
+
+    public final Scanner input = new Scanner(System.in);
+
+    public Grid() {
+        System.out.println("Introduce row numbers: ");
+        this.rows = input.nextInt();
+        System.out.println("Introduce col numbers: ");
+        this.cols = input.nextInt();
+        this.board = new int[rows][cols];
     }
 
-    public void buildGrid() {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                grid[i][j] = ".";
-                System.out.print(grid[i][j] + "\s");
+    public int[][] createBoard() {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                board[row][col] = new Cell().getState();
+            }
+        }
+        return board;
+    }
+
+    public void printBoard(int [][] inputBoard) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (inputBoard[row][col] == 0) {
+                    System.out.print("\s.\s");
+                } else {
+                    System.out.print("\s*\s");
+
+                }
+
             }
             System.out.println();
         }
     }
+    public int getCellStatus(int x, int y) {
+        if(x <0 || x >= rows){
+            return 0;
+        }
+        if(y <0 || y >= cols){
+            return 0;
+        }
+        return board[x][y];
+    }
 
+    public int aliveNeighboursCounter(int x, int y){
+        int aliveNeighboursCounter = 0;
 
-    public static void main(String[] args){
-        Grid grid = new Grid(4,4);
-        grid.buildGrid();
+        aliveNeighboursCounter +=getCellStatus(x+1, y-1);
+        aliveNeighboursCounter +=getCellStatus(x+1,y);
+        aliveNeighboursCounter +=getCellStatus(x+1, y + 1);
+        aliveNeighboursCounter +=getCellStatus(x, y + 1);
+        aliveNeighboursCounter +=getCellStatus(x,y-1);
+        aliveNeighboursCounter +=getCellStatus(x-1,y+1);
+        aliveNeighboursCounter +=getCellStatus(x-1,y);
+        aliveNeighboursCounter +=getCellStatus(x-1,y-1);
+        return aliveNeighboursCounter;
+    }
+    public void nextGeneration() {
+        int[][] nextGeneration = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                int aliveNeighbours = aliveNeighboursCounter(row,col);
+
+                if(getCellStatus(row,col) == 1){
+                    if(aliveNeighbours <2){
+                        nextGeneration[row][col] = 0;
+                    } else if (aliveNeighbours > 3) {
+                        nextGeneration[row][col] = 0;
+                    } else if (aliveNeighbours == 2 || aliveNeighbours == 3) {
+                        nextGeneration[row][col] =1;
+                    }
+                }else {
+                    if (aliveNeighbours == 3){
+                        nextGeneration[row][col] =1;
+                    }
+                }
+            }
+        }
+        board = nextGeneration;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public static void main(String[] args) {
+        Grid grid = new Grid();
+        grid.printBoard(grid.createBoard());
+        System.out.println("-----------------------------------------------\n");
+        grid.nextGeneration();
+        grid.printBoard(grid.getBoard());
+        System.out.println("-----------------------------------------------\n");
+        grid.nextGeneration();
+        grid.printBoard(grid.getBoard());
 
     }
-}
+    }
